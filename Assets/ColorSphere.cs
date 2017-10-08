3deb4a763e78e51d0a3f04afda4d2c737f266523
jess2422 <jh56@princeton.edu>
2017-10-08 17:57:36 -0400

    last

---
+++ Assets/ColorSphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSphere : MonoBehaviour {
+    Renderer rend;
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject main = GameObject.FindGameObjectWithTag("MainCamera");
+        Color currColor = main.GetComponent<Renderer>().material.GetColor("_Color");
+        rend.material.SetColor("_Color", Color.Lerp(rend.material.color, RotateTrigger.currColor, Time.deltaTime * 2f));
+    }
+}
